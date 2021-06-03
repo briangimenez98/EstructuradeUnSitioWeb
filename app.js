ef84506caac1a1c130b9496e18e0228843f4dc77
briangimenez98 <brian.gim98@gmail.com>
2021-06-03 19:20:49 -0300

    fixed html and app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const port = 3030
 const path = require('path');
 
+app.use(express.static('public/images'));
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
 })
 app.listen(3030, ()=> {
     console.log("Puerto 3030 funcionando");
 });
-app.use(express.static('public'));
